a2280390cff4a39d63d1dae068cae7c3c0b6e495
davidsmorton <davidsmorton@gmail.com>
2020-09-28 21:05:20 +0000

    adding /urls.json

--- express_server.js
+++ express_server.js
@@ -6,9 +6,13 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK" : "http://www.google.com"
 };
-
+// this is how you make the server see "/" (root), "/urls.json" //page on root
 app.get("/", (req, res) => {
   res.send("Hello");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
 
 app.listen(PORT, () => {
