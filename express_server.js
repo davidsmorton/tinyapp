c4c3dd08ec4a772cafd223ff2270cffb85f262ec
davidsmorton <davidsmorton@gmail.com>
2020-09-28 23:13:12 +0000

    made changes to urls_index per instructions

--- express_server.js
+++ express_server.js
@@ -22,6 +22,11 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+//grabing data base from above?
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on ${PORT}!`);
