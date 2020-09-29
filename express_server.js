ab25e3b76b3e9fc8ed5df2dca8e399ca39b2ba16
davidsmorton <davidsmorton@gmail.com>
2020-09-29 03:09:36 +0000

    added form for URL entry by Useradded function to randomly gen a 6 string alphaNumc char string

--- express_server.js
+++ express_server.js
@@ -1,9 +1,20 @@
+const { randomString } = require("./randomString")
 const express = require("express");
 const app = express();
 const PORT = 8080; // used as default
 
+
+
+function generateRandomString() {
+return randomString(6)
+}
+
+//console.log(generateRandomString());
+
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -29,11 +40,19 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] /* What goes here? */ };
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send(generateRandomString());         // Respond with 'Ok' (we will replace this)
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on ${PORT}!`);
---
+++ randomString.js
@@ -0,0 +1,14 @@
+function randomString(length) {
+  let text = "";
+  let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (var i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
+//console.log(randomString(6));
+
+/* reference: https://codybonney.com/generating-a-random-alphanumeric-string-with-javascript/ */
+
+module.exports = { randomString };
